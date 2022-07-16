26280f8be80b7da2cc49807e4234c28446d86143
thrkrdk <thrkardak@gmail.com>
2022-07-16 18:36:55 +0300

    added Modal Commponent with React Portal. Cart state functionality added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,18 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import classes from "./Header.module.css";
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-import mealsImage from "../../assets/meals.jpg";
-import HeaderCartButton from "./HeaderCartButton";
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Meal Basket</h1>
-        <HeaderCartButton />
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="A table full of delicious food!" />
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
